b22cb953fae1d3a238d562eb42ec8679dec6c316
unknown <aleksandarristov28@gmail.com>
2023-12-23 00:07:09 +0100

    first java program

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+
+public class MyClass {
+
+	public static void main(String[]args) {
+		vehicle111 v= new vehicle111();
+		System.out.println(v);
+		
+		
+	}
+}
---
+++ src/vehicle111.java
@@ -0,0 +1,12 @@
+
+public class vehicle111 {
+
+    public static String horn() {
+        return "Beep";
+    }
+
+    @Override
+    public String toString() {
+        return horn();
+    }
+}
